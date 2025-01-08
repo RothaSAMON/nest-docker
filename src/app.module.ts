fb58dd57d84878057b358407f29666214937ac8e
RothaSAMON <samonrotha@gmail.com>
2025-01-08 10:53:31 +0700

    initial the main include section schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     AuthModule,
+    MainModule,
   ],
   controllers: [AppController],
   providers: [AppService],
