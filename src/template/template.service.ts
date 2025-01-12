b567bd2298e02488d1851b5e169cc1499eaf090f
RothaSAMON <samonrotha@gmail.com>
2025-01-12 13:42:58 +0700

    implement the template logic

--- src/template/template.service.ts
+++ src/template/template.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Template } from './schema/template.schema';
 
 @Injectable()
-export class TemplateService {}
+export class TemplateService {
+  constructor(
+    @InjectModel(Template.name) private readonly templateModel: Model<Template>,
+  ) {}
+
+  async createTemplate(templateData: Partial<Template>): Promise<Template> {
+    const createdTemplate = new this.templateModel(templateData);
+    return createdTemplate.save();
+  }
+
+  async getAllTemplates(): Promise<Template[]> {
+    return this.templateModel.find().exec();
+  }
+
+  async getTemplateById(id: string): Promise<Template> {
+    return this.templateModel.findById(id).exec();
+  }
+
+  async updateTemplate(
+    id: string,
+    templateData: Partial<Template>,
+  ): Promise<Template> {
+    return this.templateModel
+      .findByIdAndUpdate(id, templateData, { new: true })
+      .exec();
+  }
+
+  async deleteTemplate(id: string): Promise<Template> {
+    return this.templateModel.findByIdAndDelete(id).exec();
+  }
+}
